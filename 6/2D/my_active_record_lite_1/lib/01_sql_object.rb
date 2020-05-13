20598c068a509452baf98190f811c6df8951860b
pr0grara <azbaghda@gmail.com>
2020-05-12 21:14:15 -0700

    W6D2, METAproggramming warmup

---
+++ 6/2D/my_active_record_lite_1/lib/01_sql_object.rb
@@ -0,0 +1,85 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+require 'byebug'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    # debugger
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).flatten
+      SELECT 
+        *
+      FROM 
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name 
+  end
+
+  def self.table_name
+    @table_name || self.name.underscore.pluralize
+  end
+
+  def self.all
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
