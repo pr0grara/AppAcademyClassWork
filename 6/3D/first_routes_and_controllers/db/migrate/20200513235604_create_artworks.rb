223b4a24634140acd298c5a11d19214410b51f21
pr0grara <azbaghda@gmail.com>
2020-05-13 17:05:09 -0700

    W6D3, first work with controllers in rail

---
+++ 6/3D/first_routes_and_controllers/db/migrate/20200513235604_create_artworks.rb
@@ -0,0 +1,11 @@
+class CreateArtworks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artworks do |t|
+      t.string :title, null: false
+      t.text :image_url, null: false
+      t.integer :artist_id, null: false
+    end
+    add_index :artworks, :title, unique: true
+    add_index :artworks, :artist_id, unique: true
+  end
+end
